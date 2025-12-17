6b4c83aa95be87d93a617afd54e21616216f993f
Khim Limbona <126409808+GittyStud@users.noreply.github.com>
2025-12-17 08:48:58 +0800

    Delete Warehouse Management System directory

--- Warehouse Management System/wims-backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController'); // Corrected Path
-
-router.post('/register', authController.register); // New Endpoint
-router.post('/login', authController.login);
-
-module.exports = router;
